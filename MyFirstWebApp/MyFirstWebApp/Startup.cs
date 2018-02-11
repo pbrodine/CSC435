f965d6b03b95b40a5f5dc0810c93269ff63ee957
Paul Brodine <brodinep@hotmail.com>
2018-02-11 15:30:10 -0600

    Default Controller added Default View added Registration View added in Default Changed Startup route

--- MyFirstWebApp/MyFirstWebApp/Startup.cs
+++ MyFirstWebApp/MyFirstWebApp/Startup.cs
@@ -43,7 +43,7 @@ namespace MyFirstWebApp
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Default}/{action=Registration}/{id?}");
             });
         }
     }
