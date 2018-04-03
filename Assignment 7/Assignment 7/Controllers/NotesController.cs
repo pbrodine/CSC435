a7cf0036e7105018cf4b66bbc35142335f8ab4c0
Paul Brodine <brodinep@hotmail.com>
2018-04-03 18:40:29 -0500

    Assignment 7

---
+++ Assignment 7/Assignment 7/Controllers/NotesController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Assignment_7.Models;
+
+namespace Assignment_7.Controllers
+{
+    public class NotesController : Controller
+    {
+        private readonly UserContext _context;
+
+        public NotesController(UserContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Notes
+        public async Task<IActionResult> Index()
+        {
+            var userContext = _context.Notes2.Include(n => n.Category).Include(n => n.User);
+            return View(await userContext.ToListAsync());
+        }
+
+        // GET: Notes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notes = await _context.Notes2
+                .Include(n => n.Category)
+                .Include(n => n.User)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (notes == null)
+            {
+                return NotFound();
+            }
+
+            return View(notes);
+        }
+
+        // GET: Notes/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Notes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Note,CreatedOn,CategoryId,IsDeleted,UserId")] Notes notes)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(notes);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", notes.CategoryId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", notes.UserId);
+            return View(notes);
+        }
+
+        // GET: Notes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notes = await _context.Notes2.SingleOrDefaultAsync(m => m.Id == id);
+            if (notes == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", notes.CategoryId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", notes.UserId);
+            return View(notes);
+        }
+
+        // POST: Notes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Note,CreatedOn,CategoryId,IsDeleted,UserId")] Notes notes)
+        {
+            if (id != notes.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(notes);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NotesExists(notes.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", notes.CategoryId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", notes.UserId);
+            return View(notes);
+        }
+
+        // GET: Notes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notes = await _context.Notes2
+                .Include(n => n.Category)
+                .Include(n => n.User)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (notes == null)
+            {
+                return NotFound();
+            }
+
+            return View(notes);
+        }
+
+        // POST: Notes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var notes = await _context.Notes2.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Notes2.Remove(notes);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NotesExists(int id)
+        {
+            return _context.Notes2.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Assignment 7/Assignment 7/Models/Model.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Assignment_7.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Notes> Notes2 { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string CreatedOn { get; set; }
+
+        public List<Notes> Notes2 { get; set; }
+    }
+    
+    public class Notes
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Note { get; set; }
+        public string CreatedOn { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsDeleted { get; set; }
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+        public Category Category { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Notes> Notes2 { get; set; }
+    }
+}
